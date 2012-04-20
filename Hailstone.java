603b1195db26ad9efa57d5213050646d7c574e5d
S <s>
2012-04-19 19:44:30 -0700

    {"type":"TI", "runs":"0"}

--- Hailstone.java
+++ Hailstone.java
@@ -14,33 +14,49 @@ import acm.program.*;
 public class Hailstone extends ConsoleProgram {
 
 	public void run() {
-		println("This program computes the Hailstone sequences.");
+		println("This program computes the Hailstone sequences.");					//prompts the user for input
 		int counter = 0;
 		int n = readInt("Enter a number: ");
 
 		while (n > 1) {
-			if (isEven(n)) {
-				int half = takeHalf(n);
-				println(n + " is even, so I take half = " + half);
+			if (isEven(n)) {														//determines whether or not n is even
+				int half = takeHalf(n);												//takes half of n
+				println(n + " is even, so I take half = " + half);					//displays sequence for even n
 				n = half;
 			} else {
-				int threeNPlusOne = makeThreeNPlusOne(n);
-				println(n + " is odd, so I make 3n + 1 = " + threeNPlusOne);
+				int threeNPlusOne = makeThreeNPlusOne(n);							//makes 3n + 1
+				println(n + " is odd, so I make 3n + 1 = " + threeNPlusOne);		//displays sequence for odd n
 				n = threeNPlusOne;
 			}
-			counter ++;
+			counter ++;																//counts the number of steps to reach 1
 		}
 		println("The process took " + counter + " steps to reach 1.");
 	}
 
+	/**
+	 * This method determines whether or not an integer is even by testing whether or not the
+	 * remainder is equal to zero when the integer is divided by two.
+	 * @param x This represents the integer
+	 * @return This method returns a boolean, which is true if the integer is even, and false otherwise.
+	 */
 	private boolean isEven (int x) {
 		return x % 2 == 0;
 	}
 
+	/**
+	 * This method takes in an integer and divides it by two.
+	 * @param x This represents the integer to be divided in half
+	 * @return This method returns an integer that is half of the original integer
+	 */
 	private int takeHalf (int x) {
 		return x / 2;
 	}
 
+	/**
+	 * This method takes in an integer and multiplies it by three, and then adds one to it.
+	 * @param x This represents the integer to be evaluated
+	 * @return This method returns an integer that is three times plus one more than the original value
+	 */
 	private int makeThreeNPlusOne(int x) {
 		return (3 * x) + 1;
 	} 
