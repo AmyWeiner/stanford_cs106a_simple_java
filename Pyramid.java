d51329a43cbfcbfccd9068ec25c35c9abba731b0
S <s>
2012-04-20 09:10:29 -0700

    {"type":"TI", "runs":"1"}

--- Pyramid.java
+++ Pyramid.java
@@ -15,13 +15,15 @@ public class Pyramid extends GraphicsProgram {
 	private static final int BRICKS_IN_BASE =12;
 
 	public void run() {
-	
+
 		for (int i = 0; i < BRICKS_IN_BASE; i++){
 			for (int j = BRICKS_IN_BASE; j > 0; j--) {
-				double x = (getWidth() - (BRICK_WIDTH * BRICKS_IN_BASE)) / 2 + (BRICK_WIDTH * i);
-				double y = getHeight() - (BRICK_HEIGHT * i);
-				GRect brick = new GRect (x, y, BRICK_WIDTH, BRICK_HEIGHT);
-				add(brick);
+				for (int k = 12; k > 0; k--) {
+					double x = (getWidth() - (BRICK_WIDTH * k)) / 2 + (BRICK_WIDTH * i);
+					double y = getHeight() - (BRICK_HEIGHT * i);
+					GRect brick = new GRect (x, y, BRICK_WIDTH, BRICK_HEIGHT);
+					add(brick);
+				}
 			}
 		}
 	}
