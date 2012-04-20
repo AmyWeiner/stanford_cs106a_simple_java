4d587e0fb34bca1ae25897bb9a659df42dd513ed
S <s>
2012-04-19 19:04:36 -0700

    {"type":"TI", "runs":"1"}

--- FindRange.java
+++ FindRange.java
@@ -12,6 +12,12 @@ public class FindRange extends ConsoleProgram {
 	public void run() {
 		println("This program finds the smallest and largest integers in a list.");
 		println("Enter values, one per line, using a 0 to signal the end of the list.");
+		while (true) {
+			int x = readInt("? ");
+			int y = readInt("? ");
+			int min = Math.min(x,y);
+			int max = Math.max(x, y);
+		}
 	}
 	
 	private static final int SENTINEL = 0;
--- Hailstone.java
+++ Hailstone.java
@@ -10,7 +10,32 @@ import acm.program.*;
 public class Hailstone extends ConsoleProgram {
 
 	public void run() {
-		// You fill this in
+		println("This program computes the Hailstone sequences.");
+		int counter = 0;
+		int n = readInt("Enter a number: ");
+
+		while (n > 1) {
+			if (isEven(n)) {
+				int half = takeHalf(n);
+				println(n + "is even, so I take half = " + half);
+			} else {
+				int threeNPlusOne = makeThreeNPlusOne(n);
+				println(n + "is odd, so I make 3n + 1 = " + threeNPlusOne);
+			}
+			counter ++;
+		}
+		println("The process took" + counter + "steps to reach 1.");
 	}
 
+	private boolean isEven (int x) {
+		return x % 2 == 0;
+	}
+
+	private int takeHalf (int x) {
+		return x / 2;
+	}
+
+	private int makeThreeNPlusOne(int x) {
+		return (3 * x) + 1;
+	} 
 }
