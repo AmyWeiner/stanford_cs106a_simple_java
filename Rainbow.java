46cde584c6e6f43a2899a94ef46a209e0dfc9ba8
S <s>
2012-04-21 22:28:02 -0700

    {"type":"TI", "runs":"1"}

--- Rainbow.java
+++ Rainbow.java
@@ -29,7 +29,7 @@ public class Rainbow extends GraphicsProgram {
 	}
 	
 	private void drawRedStripe() {
-		add(createFilledCircle(400, 300, 100, Color.RED));
+		add(createFilledCircle(350, 300, 100, Color.RED));
 	}
 	
 	private GOval createFilledCircle(double x, double y, double r, Color color) {
