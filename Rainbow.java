4fef19f2e264d224ee3df06b29bcf977a3ad7bc1
S <s>
2012-04-21 23:06:16 -0700

    {"type":"TI", "runs":"1"}

--- Rainbow.java
+++ Rainbow.java
@@ -28,49 +28,49 @@ public class Rainbow extends GraphicsProgram {
 	private void drawRedStripe() {
 		double cx = getWidth() / 2;
 		double cy = getHeight() / 2 + 250;
-		double r = 420;
+		double r = 460;
 		add(createFilledCircle(cx, cy, r, Color.RED));
 	}
 
 	private void drawOrangeStripe() {
 		double cx = getWidth() / 2;
 		double cy = getHeight() / 2 + 250;
-		double r = 400;
+		double r = 440;
 		add(createFilledCircle(cx, cy, r, Color.ORANGE ));
 	}
 
 	private void drawYellowStripe() {
 		double cx = getWidth() / 2;
 		double cy = getHeight() / 2 + 250;
-		double r = 380;
+		double r = 420;
 		add(createFilledCircle(cx, cy, r, Color.YELLOW));
 	}
 
 	private void drawGreenStripe() {
 		double cx = getWidth() / 2;
 		double cy = getHeight() / 2 + 250;
-		double r = 360;
+		double r = 400;
 		add(createFilledCircle(cx, cy, r, Color.GREEN));
 	}
 
 	private void drawBlueStripe() {
 		double cx = getWidth() / 2;
 		double cy = getHeight() / 2 + 250;
-		double r = 340;
+		double r = 380;
 		add(createFilledCircle(cx, cy, r, Color.BLUE));
 	}
 
 	private void drawMagentaStripe() {
 		double cx = getWidth() / 2;
 		double cy = getHeight() / 2 + 250;
-		double r = 320;
+		double r = 360;
 		add(createFilledCircle(cx, cy, r, Color.MAGENTA));
 	}
 
 	private void drawWhiteSpace() {
 		double cx = getWidth() / 2;
 		double cy = getHeight() / 2 + 250;
-		double r = 300;
+		double r = 320;
 		add(createFilledCircle(cx, cy, r, Color.WHITE));
 	}
 
