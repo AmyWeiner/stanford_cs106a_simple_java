94c06194faf2cd6d01910ab1d15be9892c790870
S <s>
2012-04-21 22:42:43 -0700

    {"type":"TI", "runs":"1"}

--- Rainbow.java
+++ Rainbow.java
@@ -13,7 +13,7 @@ public class Rainbow extends GraphicsProgram {
 	double cx = getWidth() / 2;
 	double cy = getHeight() / 2;
 	public void run() {
-		
+
 		drawRainbow();
 	}
 
@@ -25,15 +25,15 @@ public class Rainbow extends GraphicsProgram {
 		//drawBlueStripe();
 		//drawMagentaStripe();
 	}
-	
+
 	private void drawRedStripe() {
 		add(createFilledCircle(cx, cy, 20, Color.RED));
 	}
-	
+
 	private GOval createFilledCircle(double x, double y, double r, Color color) {
-		GOval circle = new GOval(x -r, y-r, 2 * r, 2 * r);
-		circle.setColor(color);
+		GOval circle = new GOval(x - r, y - r, 2 * r, 2 * r);
 		circle.setFilled(true);
+		circle.setColor(color);
 		return circle;
 	}
 }
