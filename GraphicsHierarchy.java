5ecfea32d2145bf3539be2444f27c46c2904f43c
S <s>
2012-04-22 12:20:18 -0700

    {"type":"TI", "runs":"1"}

--- GraphicsHierarchy.java
+++ GraphicsHierarchy.java
@@ -28,7 +28,7 @@ public class GraphicsHierarchy extends GraphicsProgram {
 		}
 	
 	private GRect createLabeledBox(double x, double y, double w, double h, String str) {
-		GRect rect = new GRect(x - (BOX_WIDTH / 2), y - (BOX_HEIGHT / 2), w, h);
+		GRect rect = new GRect(x - (x / 2), y - (y / 2), w, h);
 		GLabel label = new GLabel(str, x, y);
 		label.setLocation((BOX_WIDTH - label.getWidth()) / 2, (BOX_HEIGHT - (label.getAscent() + label.getDescent())) / 2);
 		add(label);
