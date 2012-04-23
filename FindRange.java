151bee771bb07d464458aa19b32ee7754ffb0ac7
S <s>
2012-04-22 19:00:30 -0700

    {"type":"TI", "runs":"4"}

--- FindRange.java
+++ FindRange.java
@@ -1,8 +1,16 @@
 /*
  * File: FindRange.java
- * --------------------
- * This program is a stub for the FindRange problem, which finds the
- * smallest and largest values in a list of integers.
+ * ----------------------------------------
+ * This program reads in a list of integers, 
+ * one per line, until the user enters the
+ * sentinel value of zero, at which point
+ * the program displays the largest and
+ * smallest values entered on the screen.
+ * if the sentinel value is entered as the
+ * first value, an error message appears.
+ * If the sentinel value is entered as the
+ * second value, the first value is displayed
+ * as both the smallest and largest values. 
  */
 
 import acm.program.*;
@@ -10,16 +18,16 @@ import acm.program.*;
 public class FindRange extends ConsoleProgram {
 
 	public void run() {
-		println("This program finds the smallest and largest integers in a list.");
-		println("Enter values, one per line, using a 0 to signal the end of the list.");
+		println("This program finds the smallest and largest integers in a list.");				//program intro
+		println("Enter values, one per line, using a 0 to signal the end of the list.");		//prompts the user to enter values
 
 		int x = readInt("? ");
 		if (x == SENTINEL) {
-			println("The Sentinel number was enetered on the first input line. Therefore, no values have been entered.");
+			println("The Sentinel number was enetered on the first input line. Therefore, no values have been entered.");		//error mesage
 		} else {
 			int y = readInt("? ");
 			if (y == SENTINEL) {
-				y = x;
+				y = x;													//sets the first value entererd as the largest
 				println("The smallest value is " + x);
 				println("The largest value is " + y);
 			}else {
@@ -38,6 +46,7 @@ public class FindRange extends ConsoleProgram {
 		}
 	}
 
+	/* Sets the value of zero as the sentinel value*/
 	private static final int SENTINEL = 0;
 
 }
