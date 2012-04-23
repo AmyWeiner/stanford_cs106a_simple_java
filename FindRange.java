a8573e315c8e6c02c81ff7126721a6872ccba9c3
S <s>
2012-04-22 17:30:57 -0700

    {"type":"TI", "runs":"1"}

--- FindRange.java
+++ FindRange.java
@@ -12,24 +12,28 @@ public class FindRange extends ConsoleProgram {
 	public void run() {
 		println("This program finds the smallest and largest integers in a list.");
 		println("Enter values, one per line, using a 0 to signal the end of the list.");
+
 		int x = readInt("? ");
 		if (x == SENTINEL) {
-		println("The Sentinel number was enetered on the first input line. Therefore, no values have been entered.");
+			println("The Sentinel number was enetered on the first input line. Therefore, no values have been entered.");
+		} else {
+			int y = readInt("? ");
+			if (y == SENTINEL) {
+				y = x;
+				println("The smallest value is " + x);
+				println("The largest value is " + y);
+			}
 		}
-		int y = readInt("? ");
-		if (y == SENTINEL) {
-			y = x;
-		}else {
-		int min = Math.min(x,y);
-		int max = Math.max(x, y);
-		println("The smallest value is " + min);
-		println("The largest value is " + max);
-		}
-		while (true) {
-			
-		}
+		//int min = Math.min(x,y);
+		//int max = Math.max(x, y);
+		//println("The smallest value is " + min);
+		//println("The largest value is " + max);
+	//}
+	while (true) {
+
 	}
-	
-	private static final int SENTINEL = 0;
+}
+
+private static final int SENTINEL = 0;
 
 }
